fd0b12df5fb7ec715ae5f3bbe8213426e0592865
IqbalAR <iqbalarauf.official@gmail.com>
2025-10-22 12:05:15 +0700

    Add gitignore and env

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,23 @@
 const { defineConfig } = require("cypress");
+const mochawesome = require('cypress-mochawesome-reporter/plugin');
 const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = defineConfig({
   reporter: "mochawesome",
   reporterOptions: {
-    reportDir: "cypress/reports/html",   // Folder output
-    overwrite: false,
-    html: false,                          // Jangan generate HTML langsung dari setiap file
-    json: true
+    charts: true,
+    reportPageTitle: 'My Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
   },
   e2e: {
     env: {
       ...process.env
     },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-
+      mochawesome(on);
+      return config;
     },
   },
   video: true,
